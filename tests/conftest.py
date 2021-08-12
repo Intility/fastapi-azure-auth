2fc0079c35004cac7b25ad2cc4606274f897de22
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2021-08-12 17:02:23 +0200

    Add tests for valid token, and with wrong signing key.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,18 @@
-import main
 import pytest
 from aioresponses import aioresponses
-from fastapi.testclient import TestClient
 from tests.utils import build_openid_keys
 
+from intility_auth_fastapi.provider_config import provider_config
 
-@pytest.fixture(scope='module')
-def test_app():
+
+@pytest.fixture
+def mock_responses():
+    provider_config.tenant_id = 'intility_tenant_id'
     with aioresponses() as mock:
         mock.get(
-            'https://login.microsoftonline.com/9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6/v2.0/.well-known/openid-configuration',
+            'https://login.microsoftonline.com/intility_tenant_id/v2.0/.well-known/openid-configuration',
             payload={
-                'token_endpoint': 'https://login.microsoftonline.com//token',
+                'token_endpoint': 'https://login.microsoftonline.com/intility_tenant_id/token',
                 'token_endpoint_auth_methods_supported': [
                     'client_secret_post',
                     'private_key_jwt',
@@ -23,7 +24,7 @@ def test_app():
                 'id_token_signing_alg_values_supported': ['RS256'],
                 'response_types_supported': ['code', 'id_token', 'code id_token', 'token id_token', 'token'],
                 'scopes_supported': ['openid'],
-                'issuer': 'https://sts.windows.net/intility_tenant_id',
+                'issuer': 'https://sts.windows.net/intility_tenant_id/',
                 'microsoft_multi_refresh_token': True,
                 'authorization_endpoint': 'https://login.microsoftonline.com/intility_tenant_idoauth2/authorize',
                 'device_authorization_endpoint': 'https://login.microsoftonline.com/intility_tenant_idoauth2/devicecode',
@@ -63,8 +64,4 @@ def test_app():
             'https://login.microsoftonline.com/common/discovery/keys',
             payload=build_openid_keys(),
         )
-        # set up (this happens before the test you're running actually runs)
-        with TestClient(main.app) as test_client:
-            # When the yield is done, your test is executed and has access to `test_client`
-            yield test_client
-        # tear down (code here is called after the test is executed)
+        yield mock
--- tests/test_validate_token.py
+++ tests/test_validate_token.py
@@ -1,14 +1,13 @@
 import pytest
-
-from intility_auth_fastapi.auth import IntilityAuthorizationCodeBearer
+from httpx import AsyncClient
+from main import app
+from tests.utils import build_access_token_azure_not_guest
 
 
 @pytest.mark.asyncio
-async def test_test(test_app):
-    intility_scheme = IntilityAuthorizationCodeBearer(
-        app=test_app,
-        app_client_id='app_client_id',
-        scopes={
-            f'api://app_client_id/user_impersonation': 'awesome description',
-        },
-    )
+async def test_root(mock_responses):
+    async with AsyncClient(
+        app=app, base_url='http://test', headers={'Authorization': 'Bearer ' + build_access_token_azure_not_guest()}
+    ) as ac:
+        response = await ac.get('api/v1/hello')
+    assert response.json() == {'hello': 'world'}
