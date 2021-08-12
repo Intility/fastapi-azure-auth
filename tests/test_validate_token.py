1e590a7a0ec78300a3c32a8e91e9d152cf391011
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2021-08-12 18:26:51 +0200

    - Fix wrong response for guest users - Add test for guest user token

--- tests/test_validate_token.py
+++ tests/test_validate_token.py
@@ -1,13 +1,35 @@
 import pytest
+from demoproj.core.config import settings
 from httpx import AsyncClient
-from main import app
-from tests.utils import build_access_token_azure_not_guest
+from main import app, intility_scheme
+from tests.utils import build_access_token_azure_guest, build_access_token_azure_not_guest
+
+from intility_auth_fastapi.auth import IntilityAuthorizationCodeBearer
 
 
 @pytest.mark.asyncio
-async def test_root(mock_responses):
+async def test_normal_user(mock_responses):
     async with AsyncClient(
         app=app, base_url='http://test', headers={'Authorization': 'Bearer ' + build_access_token_azure_not_guest()}
     ) as ac:
         response = await ac.get('api/v1/hello')
     assert response.json() == {'hello': 'world'}
+
+
+@pytest.mark.asyncio
+async def test_guest_user(mock_responses):
+    intility_scheme_no_guest = IntilityAuthorizationCodeBearer(
+        app=app,
+        app_client_id=settings.APP_CLIENT_ID,
+        scopes={
+            f'api://{settings.APP_CLIENT_ID}/user_impersonation': '**No client secret needed, leave blank**',
+        },
+        allow_guest_users=False,
+    )
+    app.dependency_overrides[intility_scheme] = intility_scheme_no_guest
+    async with AsyncClient(
+        app=app, base_url='http://test', headers={'Authorization': 'Bearer ' + build_access_token_azure_guest()}
+    ) as ac:
+        response = await ac.get('api/v1/hello')
+    print(f'{response.json()=}')
+    assert response.json() == {'detail': 'Guest users not allowed'}
