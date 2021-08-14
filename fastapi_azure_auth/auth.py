0cb07ec79ec4680833b91b1e1472e719b9a223ad
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2021-08-14 10:36:00 +0200

    Add docstring to InvalidAuth Exception

--- fastapi_azure_auth/auth.py
+++ fastapi_azure_auth/auth.py
@@ -14,6 +14,10 @@ log = logging.getLogger('fastapi_azure_auth')
 
 
 class InvalidAuth(HTTPException):
+    """
+    Exception raised when the user is not authorized
+    """
+
     def __init__(self, detail: str) -> None:
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED, detail=detail, headers={'WWW-Authenticate': 'Bearer'}
