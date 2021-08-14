7fcace888f2ee34887893f0315efd9d480c0204d
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2021-08-14 20:32:53 +0200

    Add user object to the request. Dependency now also return a user object.

--- demoproj/schemas/hello_world.py
+++ demoproj/schemas/hello_world.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field
 
+from fastapi_azure_auth.user import User
+
 
 class HelloWorldResponse(BaseModel):
     hello: str = Field(..., description='What we\'re saying hello to')
+    user: User = Field(..., description='The user object')
