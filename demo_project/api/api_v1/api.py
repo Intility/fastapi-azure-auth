595b18153db535912243ad83ffdf516ee56bc6dd
JonasKs <JonasKs@users.noreply.github.com>
2021-08-30 20:12:35 +0000

    deploy: 96a89b7658e7bacffd6941e7f57544f65d63b648

--- demo_project/api/api_v1/api.py
+++
@@ -1,5 +0,0 @@
-from demo_project.api.api_v1.endpoints import hello_world
-from fastapi import APIRouter
-
-api_router = APIRouter(tags=['hello'])
-api_router.include_router(hello_world.router)
--- fastapi_azure_auth/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi_azure_auth.auth import (  # noqa
-    MultiTenantAzureAuthorizationCodeBearer,
-    SingleTenantAzureAuthorizationCodeBearer,
-)
-
-__version__ = '3.0.0-beta1'
