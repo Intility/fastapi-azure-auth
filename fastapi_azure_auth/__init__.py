8663c821e60a4620e6bcb52f28bf3aca9748259e
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2021-09-03 10:37:40 +0200

    Add logo to README.md Add re-export of imports in __iniy__.py. This solves mypy errors for library users using `--no-implicit-reexport`.

--- fastapi_azure_auth/__init__.py
+++ fastapi_azure_auth/__init__.py
@@ -1,6 +1,6 @@
-from fastapi_azure_auth.auth import (  # noqa
-    MultiTenantAzureAuthorizationCodeBearer,
-    SingleTenantAzureAuthorizationCodeBearer,
+from fastapi_azure_auth.auth import (  # noqa: F401
+    MultiTenantAzureAuthorizationCodeBearer as MultiTenantAzureAuthorizationCodeBearer,
+    SingleTenantAzureAuthorizationCodeBearer as SingleTenantAzureAuthorizationCodeBearer,
 )
 
 __version__ = '3.0.0'
