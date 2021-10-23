e716de9cf5d914af1e969fd60e578fcd7c36c571
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2021-10-23 13:24:21 +0200

    - Support `trio` by replacing `aiohttp` with `httpx` - Remove unused `end_session_endpoint` - Rewrite tests

--- tests/multi_tenant/conftest.py
+++ tests/multi_tenant/conftest.py
@@ -1,5 +1,5 @@
+import httpx
 import pytest
-from aioresponses import aioresponses
 from demo_project.api.dependencies import azure_scheme
 from demo_project.core.config import settings
 from demo_project.main import app
@@ -27,58 +27,39 @@ def multi_tenant_app():
 
 
 @pytest.fixture
-def mock_openid():
-    with aioresponses() as mock:
-        mock.get(
-            openid_config_url(version=2, multi_tenant=True),
-            payload=openid_configuration(version=2),
-        )
-        yield mock
+def mock_openid(respx_mock):
+    respx_mock.get(openid_config_url(version=2, multi_tenant=True)).respond(json=openid_configuration(version=2))
+    yield
 
 
 @pytest.fixture
-def mock_openid_and_keys(mock_openid):
-    mock_openid.get(
-        keys_url(version=2),
-        payload=build_openid_keys(),
-    )
-    yield mock_openid
+def mock_openid_and_keys(respx_mock, mock_openid):
+    respx_mock.get(keys_url(version=2)).respond(json=build_openid_keys())
+    yield
 
 
 @pytest.fixture
-def mock_openid_and_empty_keys(mock_openid):
-    mock_openid.get(
-        keys_url(version=2),
-        payload=build_openid_keys(empty_keys=True),
-    )
-    yield mock_openid
+def mock_openid_and_empty_keys(respx_mock, mock_openid):
+    respx_mock.get(keys_url(version=2)).respond(json=build_openid_keys(empty_keys=True))
+    yield
 
 
 @pytest.fixture
-def mock_openid_ok_then_empty(mock_openid):
-    mock_openid.get(
-        keys_url(version=2),
-        payload=build_openid_keys(),
-    )
-    mock_openid.get(
-        keys_url(version=2),
-        payload=build_openid_keys(empty_keys=True),
-    )
-    mock_openid.get(
-        openid_config_url(version=2, multi_tenant=True),
-        payload=openid_configuration(version=2),
-    )
-    mock_openid.get(
-        openid_config_url(version=2, multi_tenant=True),
-        payload=openid_configuration(version=2),
-    )
-    yield mock_openid
+def mock_openid_ok_then_empty(respx_mock, mock_openid):
+    keys_route = respx_mock.get(keys_url(version=2))
+    keys_route.side_effect = [
+        httpx.Response(json=build_openid_keys(), status_code=200),
+        httpx.Response(json=build_openid_keys(empty_keys=True), status_code=200),
+    ]
+    openid_route = respx_mock.get(openid_config_url(version=2, multi_tenant=True))
+    openid_route.side_effect = [
+        httpx.Response(json=openid_configuration(version=2), status_code=200),
+        httpx.Response(json=openid_configuration(version=2), status_code=200),
+    ]
+    yield
 
 
 @pytest.fixture
-def mock_openid_and_no_valid_keys(mock_openid):
-    mock_openid.get(
-        keys_url(version=2),
-        payload=build_openid_keys(no_valid_keys=True),
-    )
-    yield mock_openid
+def mock_openid_and_no_valid_keys(respx_mock, mock_openid):
+    respx_mock.get(keys_url(version=2)).respond(json=build_openid_keys(no_valid_keys=True))
+    yield
