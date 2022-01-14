adee7edff7739daf92d977851825eae26fc6ebed
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2022-01-14 11:23:47 +0100

    Add example API with multiple authentication options, namely Azure and API key auth

---
+++ demo_project/api/api_v1/endpoints/hello_world_multi_auth.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+from demo_project.api.dependencies import multi_auth
+from demo_project.schemas.hello_world import TokenType
+from fastapi import APIRouter, Depends, Request
+
+from fastapi_azure_auth.user import User
+
+router = APIRouter()
+
+
+@router.get(
+    '/hello-multi-auth',
+    response_model=TokenType,
+    summary='Say hello with an API key',
+    name='hello_world_api_key',
+    operation_id='helloWorldApiKey',
+)
+async def world(request: Request, auth: Union[str, User] = Depends(multi_auth)) -> dict[str, bool]:
+    """
+    Wonder how this auth is done?
+    """
+    if isinstance(auth, str):
+        # An API key was used
+        return {'api_key': True, 'azure_auth': False}
+    return {'api_key': False, 'azure_auth': True}
--- demo_project/schemas/hello_world.py
+++ demo_project/schemas/hello_world.py
@@ -6,3 +6,8 @@ from fastapi_azure_auth.user import User
 class HelloWorldResponse(BaseModel):
     hello: str = Field(..., description='What we\'re saying hello to')
     user: User = Field(..., description='The user object')
+
+
+class TokenType(BaseModel):
+    api_key: bool = Field(..., description='API key was used')
+    azure_auth: bool = Field(..., description='Azure auth was used')
