3702e935e3be49864c8a89e06daee54337ac6d73
Jonas Krüger Svensson <jonas.svensson@intility.no>
2022-04-04 20:28:54 +0200

    Fix tests and warnings in tests

--- demo_project/api/api_v1/endpoints/graph.py
+++ demo_project/api/api_v1/endpoints/graph.py
@@ -17,7 +17,7 @@ router = APIRouter()
     operation_id='helloGraph',
     dependencies=[Depends(azure_scheme)],
 )
-async def graph_world(request: Request) -> Any:
+async def graph_world(request: Request) -> Any:  # noqa: ANN401
     """
     An example on how to use "on behalf of"-flow to fetch a graph token and then fetch data from graph.
     """
@@ -37,17 +37,17 @@ async def graph_world(request: Request) -> Any:
 
         if obo_response.is_success:
             # Call the graph `/me` endpoint to fetch more information about the current user, using the new token
-            graph_response = httpx.Response = await client.get(
+            graph_response: httpx.Response = await client.get(
                 'https://graph.microsoft.com/v1.0/me',
                 headers={'Authorization': f'Bearer {obo_response.json()["access_token"]}'},
             )
-            graph_response = graph_response.json()
+            graph = graph_response.json()
         else:
-            graph_response = 'skipped'
+            graph = 'skipped'
 
         # Return all the information to the end user
         return (
             {'claims': jwt.get_unverified_claims(token=request.state.user.access_token)}
             | {'obo_response': obo_response.json()}
-            | {'graph_response': graph_response}
+            | {'graph_response': graph}
         )
