8c4c366c005da53a9543afff19c6419e937966d9
Jonas Krüger Svensson <jonas.svensson@intility.no>
2022-04-04 20:10:55 +0200

    Close #62, #61, #45, #40   - #62: Tutorial on how to use Python to interact with your APIs   - #61: Tutorial on how to access user attributes in your APIs   - #45: Added a page that explains how to solve admin consent when     signing in. Thank you @bkmetzler   - #40: Add tutorial on how to set up and use Graph APIs using the On     Behalf Flow. Thank you @u-iandono & @h3rmanj - Add graph endpoint example

--- demo_project/api/api_v1/endpoints/graph.py
+++ demo_project/api/api_v1/endpoints/graph.py
@@ -1,11 +1,11 @@
-from typing import Any, Union
+from typing import Any
 
+import httpx
 from demo_project.api.dependencies import azure_scheme
 from demo_project.core.config import settings
 from fastapi import APIRouter, Depends, Request
 from httpx import AsyncClient
-
-from fastapi_azure_auth.user import User
+from jose import jwt
 
 router = APIRouter()
 
@@ -15,22 +15,39 @@ router = APIRouter()
     summary='Fetch graph API using OBO',
     name='graph',
     operation_id='helloGraph',
+    dependencies=[Depends(azure_scheme)],
 )
-async def world(request: Request, auth: Union[str, User] = Depends(azure_scheme)) -> Any:
+async def graph_world(request: Request) -> Any:
     """
-    Wonder how this auth is done?
+    An example on how to use "on behalf of"-flow to fetch a graph token and then fetch data from graph.
     """
-    print(f'{request.state.user.access_token=}')  # noqa
     async with AsyncClient() as client:
-        y = {
-            'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-            'client_id': settings.APP_CLIENT_ID,
-            'client_secret': settings.GRAPH_SECRET,
-            'assertion': request.state.user.access_token,
-            'scope': 'https://graph.microsoft.com/user.read',
-            'requested_token_use': 'on_behalf_of',
-        }
-        response = await client.post(
-            'https://login.microsoftonline.com/9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6/oauth2/v2.0/token', data=y
+        # Use the users access token and fetch a new access token for the Graph API
+        obo_response: httpx.Response = await client.post(
+            f'https://login.microsoftonline.com/{settings.TENANT_ID}/oauth2/v2.0/token',
+            data={
+                'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'client_id': settings.APP_CLIENT_ID,
+                'client_secret': settings.GRAPH_SECRET,
+                'assertion': request.state.user.access_token,
+                'scope': 'https://graph.microsoft.com/user.read',
+                'requested_token_use': 'on_behalf_of',
+            },
         )
-        return {'access_token': request.state.user.access_token} | response.json()
+
+        if obo_response.is_success:
+            # Call the graph `/me` endpoint to fetch more information about the current user, using the new token
+            graph_response = httpx.Response = await client.get(
+                'https://graph.microsoft.com/v1.0/me',
+                headers={'Authorization': f'Bearer {obo_response.json()["access_token"]}'},
+            )
+            graph_response = graph_response.json()
+        else:
+            graph_response = 'skipped'
+
+        # Return all the information to the end user
+        return (
+            {'claims': jwt.get_unverified_claims(token=request.state.user.access_token)}
+            | {'obo_response': obo_response.json()}
+            | {'graph_response': graph_response}
+        )
