09d3979f9cbefeb95a3e0303912ebc2528e72bce
Jonas Krüger Svensson <jonas.svensson@intility.no>
2022-11-10 14:53:10 +0100

    chore: support python3.11

--- fastapi_azure_auth/__init__.py
+++ fastapi_azure_auth/__init__.py
@@ -4,4 +4,4 @@ from fastapi_azure_auth.auth import (  # noqa: F401
     SingleTenantAzureAuthorizationCodeBearer as SingleTenantAzureAuthorizationCodeBearer,
 )
 
-__version__ = '3.5.0'
+__version__ = '3.5.1'
