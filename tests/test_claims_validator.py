717fd3578ad6c6f14a23b572070b4ba8d7c75b90
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-05-10 13:26:17 +0200

    Fix lint errors

--- tests/test_claims_validator.py
+++ tests/test_claims_validator.py
@@ -1,36 +1,36 @@
 import pytest
+from tests.utils import do_build_claims
 
 from fastapi_azure_auth.user import Claims
-from tests.utils import do_build_claims
 
 
 def test_valid_v1_claims():
     claims = Claims(**do_build_claims(tenant_id='intility_tenant_id', version=1))
 
-    assert claims.unique_name == "jonas"
+    assert claims.unique_name == 'jonas'
     assert claims.preferred_username is None
-    assert claims.ver == "1.0"
+    assert claims.ver == '1.0'
 
 
 def test_valid_v2_claims():
     claims = Claims(**do_build_claims(tenant_id='intility_tenant_id', version=2))
 
     assert claims.unique_name is None
-    assert claims.preferred_username == "jonas.svensson@intility.no"
-    assert claims.ver == "2.0"
+    assert claims.preferred_username == 'jonas.svensson@intility.no'
+    assert claims.ver == '2.0'
 
 
 def test_invalid_v1_claims():
     claims = do_build_claims(tenant_id='intility_tenant_id', version=1)
-    claims["ver"] = "2.0"
+    claims['ver'] = '2.0'
 
-    with pytest.raises(ValueError, match="only available in V1.0 tokens"):
+    with pytest.raises(ValueError, match='only available in V1.0 tokens'):
         Claims(**claims)
 
 
 def test_invalid_v2_claims():
     claims = do_build_claims(tenant_id='intility_tenant_id', version=2)
-    claims["ver"] = "1.0"
+    claims['ver'] = '1.0'
 
-    with pytest.raises(ValueError, match="only available in V2.0 tokens"):
+    with pytest.raises(ValueError, match='only available in V2.0 tokens'):
         Claims(**claims)
