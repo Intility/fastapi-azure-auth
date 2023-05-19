666ff17cfc96890f7fb278fd35be5648dd952806
Jonas Krüger Svensson <jonas.svensson@intility.no>
2023-05-19 17:50:54 +0200

    chore: version bump

--- fastapi_azure_auth/__init__.py
+++ fastapi_azure_auth/__init__.py
@@ -4,4 +4,4 @@ from fastapi_azure_auth.auth import (  # noqa: F401
     SingleTenantAzureAuthorizationCodeBearer as SingleTenantAzureAuthorizationCodeBearer,
 )
 
-__version__ = '4.0.0'
+__version__ = '4.1.0'
