84c72140d3ad6a31a0c82c806d47ea6140e8e67d
ndxmrb <mrohdenburg@nordex-online.com>
2024-08-19 17:47:19 +0200

    style: linting

--- fastapi_azure_auth/exceptions.py
+++ fastapi_azure_auth/exceptions.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from fastapi import HTTPException, WebSocketException, status
 from starlette.requests import HTTPConnection
 
@@ -10,7 +11,7 @@ class InvalidAuthHttp(HTTPException):
 
     def __init__(self, detail: str) -> None:
         super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail=detail, headers={'WWW-Authenticate': 'Bearer'}
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=detail, headers={"WWW-Authenticate": "Bearer"}
         )
 
 
@@ -30,6 +31,6 @@ def InvalidAuth(detail: str, request: HTTPConnection) -> InvalidAuthHttp | Inval
     """
     Returns the correct exception based on the connection type
     """
-    if request.scope['type'] == 'http':
+    if request.scope["type"] == "http":
         return InvalidAuthHttp(detail)
     return InvalidAuthWebSocket(detail)
